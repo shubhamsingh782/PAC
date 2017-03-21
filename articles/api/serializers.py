4b5223c7e863a8c3bb96777fce4dfe1eed5d916e
shubham <tantric.singh73@gmail.com>
2017-03-21 17:54:13 +0530

    Detailed serializer

--- articles/api/serializers.py
+++ articles/api/serializers.py
@@ -41,7 +41,7 @@ class ArticleListSerializer(serializers.ModelSerializer):
 	url = serializers.HyperlinkedIdentityField(view_name='api_articles:detail')
 	class Meta:
 		model = Article
-		fields = ('url','title',)
+		fields = ('url','title','content',)
 
 
 class ArticleDetailSerializer(serializers.ModelSerializer):
