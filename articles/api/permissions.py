b5d0c23eab88a41a3e5e818b71aefc9f3fcab6f0
shubham <tantric.singh73@gmail.com>
2017-03-22 17:49:47 +0530

    error

--- articles/api/permissions.py
+++ articles/api/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permisions import BasePermission
+from rest_framework.permissions import BasePermission
 
 class IsOwner(BasePermission):
 	def has_object_permission(self, request, view, obj):
