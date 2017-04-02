3840fbb8808138a7918ee2b2c309c095463cfc67
shubham <tantric.singh73@gmail.com>
2017-04-02 23:51:08 +0530

    changes

--- articles/api/views.py
+++ articles/api/views.py
@@ -42,10 +42,9 @@ def scrape(url):
 			content+=para.text
 			content+='\n'
 	
-	html = urlopen(url)
-	art = BeautifulSoup(html.read(),'html.parser')
-	
 	if image == None or image=='':
+		html = urlopen(url)
+		art = BeautifulSoup(html.read(),'html.parser')
 		link=art.find('img')
 		src = link['src']
 		if 'https' or 'http' in src:
@@ -74,7 +73,7 @@ class UserLoginAPIView(APIView):
 		data = request.data
 		serializer = UserLoginSerializer(data=data)
 		if not serializer.is_valid():
-			return Response({'message':"Incomplete Credentials"}, status=HTTP_400_BAD_REQUEST)
+			return Response({'status':False,'message':"Incomplete Credentials"}, status=HTTP_400_BAD_REQUEST)
 
 		user = serializer.validated_data['user']
 		token, created = Token.objects.get_or_create(user=user)
