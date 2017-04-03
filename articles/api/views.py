7811b0aa81b5d3b43213f6435ce4e7f28973cca5
shubham <tantric.singh73@gmail.com>
2017-04-03 15:52:04 +0530

    blocked pages

--- articles/api/views.py
+++ articles/api/views.py
@@ -23,7 +23,22 @@ from django.contrib import auth
 from rest_framework.authtoken.models import Token
 from newspaper import Article as page
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from urllib.request import FancyURLopener
+from random import choice
+
+user_agents = [
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
+    'Opera/9.25 (Windows NT 5.1; U; en)',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+    'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+    'Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9'
+]
+
+class MyOpener(FancyURLopener):
+	version = choice(user_agents)
+
+urlopen = MyOpener().open
 
 def scrape(url):
 	html = urlopen(url)
@@ -38,7 +53,6 @@ def scrape(url):
 		image = article.top_image
 	except:
 		image = ''
-		title = art.title.text
 		content = ""
 		for para in article.find_all('p'):
 			content+=para.text
