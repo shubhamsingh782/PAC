866f32f0377a01d7bd8c958011139f0cacbeed7c
shubham <tantric.singh73@gmail.com>
2017-05-21 12:46:13 +0530

    images

--- articles/models.py
+++ articles/models.py
@@ -26,7 +26,7 @@ class Article(models.Model):
 
 
 class Profile(models.Model):
-	user = models.OneToOneField(User, related_name='user_image')
+	user = models.OneToOneField(User, related_name='user_image', default=User.objects.filter(username='shubham'))
 	photo = models.ImageField(upload_to="photos/%Y/%m/%d", blank=True)
 
 	def __str__(self):
